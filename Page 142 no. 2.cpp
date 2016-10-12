851114bbf7b4a52c7b4e670af9d48ea69fb764b1
Heyitskezia <Nathania98@gmail.com>
2016-10-12 22:06:04 +0700

    Update Page 142 no. 2.cpp

--- Page 142 no. 2.cpp
+++ Page 142 no. 2.cpp
@@ -6,7 +6,7 @@ int main()
 {
  double classA = 15, classB = 12, classC = 9 ;
  double totalSalesA, totalSalesB, totalSalesC ;
- double total;
+ double total ;
  
  cout << "Ticket price" << endl << endl ;
  cout << "Class A = $ 15" << endl ;
@@ -21,14 +21,14 @@ int main()
  cout << "Class C sold: " ;
  cin >> totalSalesC ;
 
- cout << setprecision(2) << fixed;
+ cout << setprecision(2) << fixed ;
  cout << "Total sales from Class A: $" << totalSalesA * classA << endl ;
  cout << "Total sales from Class B: $" << totalSalesB * classB << endl ;
  cout << "Total sales from Class C: $" << totalSalesC * classC << endl  ;
 
  total = (totalSalesA * classA) + (totalSalesB * classB) + (totalSalesC * classC) ;
 
- cout << setprecision(1) << fixed;
+ cout << setprecision(2) << fixed ;
  cout << "Total sales of all class: $" << total << endl ;
  
  return 0;
