fd2732ca0a36082d27fba4f03f88e1c7878667a3
Heyitskezia <Nathania98@gmail.com>
2016-10-12 13:19:53 +0700

    Page 142 no. 1.cpp

---
+++ Page 142 no. 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+	
+ double capacity, miles, average ;
+
+ cout << "Enter the number of size of the tank (gallons): " ;
+ cin >> capacity;
+ 
+ 
+ cout << "Enter the number of miles per tank of gas: " ;
+ cin >> miles ; 
+ 
+ average = miles / capacity ;
+
+ cout << "The number of miles that may be driven per gallon of gas is " << average << endl << endl ;
+
+ return 0;
+}
